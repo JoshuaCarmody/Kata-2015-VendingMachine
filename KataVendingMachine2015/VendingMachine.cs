0767f32fae7775cb5fafe9dfc59803d0b3039526
Joshua Carmody <contact@joshuacarmody.com>
2015-06-05 16:37:47 -0400

    Added test project, then added first test to see if there was an inventory, then made that test pass.

---
+++ KataVendingMachine2015/VendingMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataVendingMachine2015
+{
+    public class VendingMachine
+    {
+        private IEnumerable<Product> Inventory = new List<Product>();
+
+        public VendingMachine()
+        {
+        }
+
+        public IEnumerable<Product> GetInventory()
+        {
+            return Inventory;
+        }
+    }
+}
---
+++ KataVendingMachine2015Tests/VendingMachineTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using KataVendingMachine2015;
+using System.Collections.Generic;
+
+namespace KataVendingMachine2015Tests
+{
+    [TestClass]
+    public class VendingMachineTests
+    {
+        private VendingMachine vm = null;
+
+        [TestInitialize]
+        public void Init()
+        {
+            vm = new VendingMachine();
+        }
+
+        [TestMethod]
+        public void Vending_Machine_Should_Have_Products()
+        {
+            IEnumerable<Product> result = vm.GetInventory();
+
+            Assert.AreNotEqual(null, result);
+        }
+
+    }
+}
